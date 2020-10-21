6bfdd6d0ac5d486c8d851a61de6e5f3d0e7d7604
Papaya Delight <jasmineteters2020@gmail.com>
2020-10-20 22:01:14 -0600

    wrote out comments/steps to take in the JS code

---
+++ script.js
@@ -0,0 +1,13 @@
+// When they type in the search bar, need to take their input, prevent default, save to localstorage and put it in the URL query for API pull
+
+
+// After click, prepend search input to the card
+
+
+// After click, put city they pulled on the card on the right with todays date
+
+
+// populate the 5 day forecast in the cards below
+
+
+// this needs to function on every search input
