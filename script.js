9ee01712fb05e848125d578001f8ea9dacdfb82b
Papaya Delight <jasmineteters2020@gmail.com>
2020-10-21 23:13:34 -0600

    defined vars in JS, added event listener for the search function and appended the search item to the UL. removed the class d-none to display the weather board

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-// When they type in the search bar, need to take their input, prevent default, save to localstorage and put it in the URL query for API pull
+// Define all variables
+var cityCurrent = $(".city");
+var dateCurrent = $(".date");
+var cityList = $(".city-list");
+var iconHeader = $(".icon-header");
+var mainCardTemp = $(".main-card-temp");
+var mainCardHumidity = $(".main-card-humidity");
+var windSpeed = $(".wind-speed");
+var uvIndex = $(".uv-index");
+var m = moment();
+console.log(moment().format("dddd MMMM Do YYYY"));
 
+var cityLists = JSON.parse(localStorage.getItem("city")) || {}
+
+var cityDate = cityCurrent + dateCurrent
+
+// When they type in the search bar, need to take their input, prevent default, save to localstorage and put it in the URL query for API pull
+$("#searchBtn").on("click", searchCity);
+
+
+function searchCity(event) {
+    event.preventDefault();
+    var citySearched = $("#citySearched").val();
+    localStorage.setItem("city", JSON.stringify(cityLists))
+    console.log(citySearched);
+    $("ul").prepend("<li class='list-group-item'>" + citySearched + "</li>");
+    $("div").removeClass("d-none");
+}
 
 // After click, prepend search input to the card
 
